1f12ae93b14c9ae6b8c009e6fab9831933f3573b
korel <x.korel.x@gmail.com>
2018-10-30 17:34:02 +0100

    PROJET - Optimisation de INDEX.PHP avec les liens opérattionnels

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <!-- Le fichier Header.php est inclus sur la page -->
 <?php require_once(__DIR__.'/partials/header.php'); 
 
-// Récupère la liste des pizzas
-$query = $db -> query('SELECT * FROM movies INNER JOIN category ON movies.category_id = category.id');
+// Récupère 3 vidéos de la categorie 8 (bfa)
+// ATTENTION on renomme l'ID de MOVIES -> en 'id_movie' car confusion avec les ID
+$query = $db -> query('SELECT *, movies.id as id_movie FROM movies INNER JOIN category ON movies.category_id = category.id WHERE category_id = 8 LIMIT 3');
 $movies = $query -> fetchAll();
+
+// récupère un film avec la catégorie 8 (bfa)
+$query = $db -> query('SELECT * FROM movies WHERE category_id = 8');
+$bfa = $query -> fetch();
+
+// Récupère 3 vidéos de la categorie 7 (legion)
+// ATTENTION on renomme l'ID de MOVIES -> en 'id_movie' car confusion avec les ID
+$query = $db -> query('SELECT *, movies.id as id_movie FROM movies INNER JOIN category ON movies.category_id = category.id WHERE category_id = 7 LIMIT 3');
+$legions = $query -> fetchAll();
+
+// récupère un film avec la catégorie 7 (legion)
+$query = $db -> query('SELECT * FROM movies WHERE category_id = 7');
+$leg = $query -> fetch();
 
 ?>
 
@@ -14,11 +28,11 @@ $movies = $query -> fetchAll();
             <div class="container">
                 <div class="row">
                     <div class="col-md-9 pt-5 py-3 encart-titre">
-                        <h2 class="display-6 text-white"> Vanilla</h2>
+                        <h2 class="display-6 text-white"> <?php echo($bfa['title']);?></h2>
                         <p class="text-muted">Le jeu prend place dans le monde imaginaire d’Azeroth à son début.</p>
                     </div>
                     <div class="col-md-3 pt-5 py-3 text-right">
-                        <a href="" ><button type="button" class="btn btn-outline-light rounded-0 border-0 btn-tous">Voir toutes les vidéos</button></a>
+                        <a href="<?php echo'movie_category.php?id='. $bfa['id']; ?>" ><button type="button" class="btn btn-outline-light rounded-0 border-0 btn-tous">Voir toutes les vidéos</button></a>
                     </div>
                 </div>
                 
@@ -26,17 +40,14 @@ $movies = $query -> fetchAll();
                 
                 
                 <div class="row">
-                    <?php 
-
-                    foreach ($movies as $movie){ 
-                        if($movie['name'] === 'Vanilla'){
-
-                    ?>
+                    <?php  foreach ($movies as $movie){ ?>
                         <div class="col-md-4">
                             <div class="card mb-3 bg-transparent position-relative encart-visuel">
-                                <a href="<?= "movie_single.php?id=" . $movie['category_id'] ?>" class="filtre-sombre position-relative">
+                                <a href="<?= "movie_single.php?id=" . $movie['id_movie'] ?>" class="filtre-sombre position-relative">
                                     <div class="icone-play position-absolute text-white "><i class="fas fa-play-circle icone-web"></i></div>
-                                    <img class="card-img-top" src="assets/<?php echo($movie['cover']);?>" alt="<?php echo($movie['title']);?>">
+                                    <div class="encart-viv">
+                                        <img class="card-img-top" src="assets/<?php echo($movie['cover']);?>" alt="<?php echo($movie['title']);?>">
+                                    </div>
                                 </a>
                                 <div class="card-body p-0 d-flex">
                                     <h6 class="col-8 p-2 text-white "><?php echo($movie['title']);?></h6>
@@ -44,12 +55,10 @@ $movies = $query -> fetchAll();
                                 </div>
                             </div>   
                         </div>
-                    <?php 
-                        }
+                        <?php 
                     }
-                    ?>
+                ?>
                 </div>
-
 
             </div>
         </section>
@@ -89,35 +98,34 @@ $movies = $query -> fetchAll();
             <div class="container">
                 <div class="row">
                     <div class="col-md-9 pt-5 py- encart-titre">
-                        <h2 class="display-6 text-white">Wrath of the Lich King</h2>
+                        <h2 class="display-6 text-white"><?php echo($leg['title']);?></h2>
                         <p class="text-muted">L’extension se déroule après « The Burning Crusade ».</p>
                     </div>
                     <div class="col-md-3 pt-5 py-3 text-right">
-                        <a href="#" ><button type="button" class="btn btn-outline-light rounded-0 border-0 btn-tous">Voir toutes les vidéos</button></a>
+                        <a href="<?php echo'movie_category.php?id='. $leg['id']; ?>" ><button type="button" class="btn btn-outline-light rounded-0 border-0 btn-tous">Voir toutes les vidéos</button></a>
                     </div>
                 </div>
                 
                 
                 <div class="row">
-                <?php  foreach ($movies as $movie){ 
-                    if($movie['name'] === 'Wrath of the Lich King'){
-                ?>
-                    <div class="col-md-4">
-                        <div class="card mb-3 bg-transparent position-relative encart-visuel">
-                            <a href="<?= "movie_single.php?id=" . $movie['category_id'] ?>" class="filtre-sombre position-relative">
-                                <div class="icone-play position-absolute text-white "><i class="fas fa-play-circle icone-web"></i></div>
-                                <img class="card-img-top" src="assets/<?php echo($movie['cover']);?>" alt="<?php echo($movie['title']);?>">
-                            </a>
-                            <div class="card-body p-0 d-flex">
-                                <h6 class="col-8 p-2 text-white "><?php echo($movie['title']);?></h6>
-                                <p class="col-4 p-2 text-danger text-right"><?php echo($movie['released_at']);?></p>
-                            </div>
-                        </div>   
-                    </div>
-                <?php 
+                    <?php  foreach ($legions as $legion){ ?>
+                        <div class="col-md-4">
+                            <div class="card mb-3 bg-transparent position-relative encart-visuel">
+                                <a href="<?= "movie_single.php?id=" . $legion['id_movie'] ?>" class="filtre-sombre position-relative">
+                                    <div class="icone-play position-absolute text-white "><i class="fas fa-play-circle icone-web"></i></div>
+                                    <div class="encart-viv">
+                                        <img class="card-img-top" src="assets/<?php echo($legion['cover']);?>" alt="<?php echo($legion['title']);?>">
+                                    </div>
+                                </a>
+                                <div class="card-body p-0 d-flex">
+                                    <h6 class="col-8 p-2 text-white "><?php echo($legion['title']);?></h6>
+                                    <p class="col-4 p-2 text-danger text-right"><?php echo($legion['released_at']);?></p>
+                                </div>
+                            </div>   
+                        </div>
+                        <?php 
                     }
-                }
-                ?>      
+                ?>
                 </div>
 
                     
